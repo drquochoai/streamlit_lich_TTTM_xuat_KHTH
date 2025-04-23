c54c207f8bbb3f4e916150993b2bfbd43a4fba73
Tran Quoc Hoai <bsquochoainew@gmail.com>
2025-04-23 10:49:19 +0700

    g

--- streamlitGUI/p2_fragments_html_ct_control_buttons.py
+++ streamlitGUI/p2_fragments_html_ct_control_buttons.py
@@ -2,6 +2,7 @@ import html
 import streamlit as st
 from SETTINGS_FOR_ALL import SETTINGS
 from datetime import datetime
+import pytz
 
 
 @st.fragment
@@ -10,7 +11,8 @@ def ct_control_buttons_html_string(b64):
     # create st download_button  excel file with b64 data
     # get current date time for the name of file
     # Get the current date and time
-    current_datetime = datetime.now().strftime("%d.%m.%Y lúc %Hh%Mm%S")
+    timezone = pytz.timezone("Asia/Ho_Chi_Minh")
+    current_datetime = datetime.now(timezone).strftime("%d.%m.%Y lúc %Hh%Mm%S")
     name_of_file = SETTINGS["name_of_exported_file"] + " - "+ st.session_state.selected_sheet_name + " - "+ current_datetime + ".xlsx"
     html_string = f'''
     <a href = "data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download = "{name_of_file}"
